f094ff8b678b881e96770cb87729bf06fbcae928
dermao <15687721774@163.com>
2024-05-21 10:52:24 +0800

    init

--- src/main/java/com/iweb/blog/common/cache/CacheAspect.java
+++ src/main/java/com/iweb/blog/common/cache/CacheAspect.java
@@ -11,9 +11,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AliasFor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
